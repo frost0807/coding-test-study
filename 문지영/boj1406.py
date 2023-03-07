ffbd92711b16e37964488ab2ce0f3d1d292ccd47
moon-zzyy <gracemjy@konkuk.ac.kr>
2023-03-08 00:14:28 +0900

    230307 edit code

--- 문지영/boj1406.py
+++ 문지영/boj1406.py
@@ -3,21 +3,21 @@
 import sys
 input = sys.stdin.readline
 
-s1 = list(input().strip())
-s2 = []
+s1 = list(input().strip()) # 커서 앞
+s2 = [] # 커서 뒤
 M = int(input())
-N = len(s1)
+
 for _ in range(M):
     query = input().strip()
 
-    if query == 'P': # 추가
-        c = query.split()[1]
-        s1.append(c)
-    if query == 'L' and not s1: # s1->s2
-        s2.append(s1.pop())
-    elif query == 'D' and not s2: # s2->s1
-        s1.append(s2.pop())
-    elif query == 'B' and not s1: # 삭제
-        s1.pop()
+    if query=='L':
+        if s1: s2.append(s1.pop()) # s1->s2
+    elif query=='D':
+        if s2: s1.append(s2.pop()) # s1<-s2
+    elif query=='B':
+        if s1: s1.pop() # 삭제
+    else: # 'P'
+        s1.append(query[-1])
 
-print(''.join(s1 + list(reversed(s2))))
+s1.extend(reversed(s2))
+print(''.join(s1))
