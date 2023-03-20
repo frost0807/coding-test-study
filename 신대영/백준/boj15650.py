ea0639dd46e880e466701ddd5c12224f55b8596f
Daeyoung Shin <sdy8603@gmail.com>
2023-03-20 17:27:10 +0900

    feature: 백준 15650 문제 풀이 커잇

---
+++ 신대영/백준/boj15650.py
@@ -0,0 +1,31 @@
+from itertools import combinations
+
+n, m = int(input())
+
+arr = [0, 1, 2, 3, 4, 5]
+
+
+def gen_combinations(arr, n):
+    result = []
+
+    if n == 0:
+        return [[]]
+
+    for i in range(0, len(arr)):
+        elem = arr[i]
+        rest_arr = arr[i + 1:]
+        for C in gen_combinations(rest_arr, n-1):
+            result.append([elem]+C)
+
+    return result
+
+
+def solution(n, m):
+    l = [i for i in range(1, n + 1)]
+    # python 라이브러리 사용 조합
+    print(combinations(l, m))
+    # python 조한 함수 구현
+    print(gen_combinations(l, m))
+
+
+solution(n, m)
