d206e087f7da8c9c3dc5e63120992ab63529edae
Daeyoung Shin <sdy8603@gmail.com>
2023-03-21 18:25:27 +0900

    feature: 15652 문제 python 라이브러리 버전 추가

--- 신대영/백준/boj15652.py
+++ 신대영/백준/boj15652.py
@@ -2,8 +2,18 @@
 문제유형: 백트래킹
 문제링크: https://www.acmicpc.net/problem/15652
 """
+# python 라이브러리 구현
+from itertools import combinations_with_replacement
+
 n, m = map(int, input().split())
 num_list = [str(i) for i in range(1, n + 1)]
+
+for number in combinations_with_replacement(num_list, m):
+    print(" ".join(number))
+
+# DFS로 직접구현
+n, m = map(str, input().split())
+num_list = [i for i in range(1, n + 1)]
 result = []
 
 
