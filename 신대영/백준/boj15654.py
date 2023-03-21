d1076db6971a88b44afc3f7ad9b591640bded5e5
Daeyoung Shin <sdy8603@gmail.com>
2023-03-21 19:13:06 +0900

    feature: 백준 15654 문제 라이브러리 버전 추가

--- 신대영/백준/boj15654.py
+++ 신대영/백준/boj15654.py
@@ -2,7 +2,19 @@
 문제유형: 백트래킹
 문제링크: https://www.acmicpc.net/problem/15654
 """
+# 라이브러리 버전
+from itertools import permutations
 import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+num_list = list(map(int, input().split()))
+num_list.sort()
+
+for p in list(permutations(num_list, m)):
+    print(' '.join(map(str, list(p))))
+
+
+# 직접구현
 input = sys.stdin.readline
 n, m = map(int, input().split())
 num_list = list(map(int, input().split()))
@@ -12,11 +24,11 @@ result = []
 
 def dfs(k):
     if k == m:
-        return print(' '.join(result))
+        return print(' '.join(map(str, result)))
 
     for i in range(n):
         if not used[i]:
-            result.append(str(num_list[i]))
+            result.append(num_list[i])
             used[i] = True
             dfs(k + 1)
             used[i] = False
