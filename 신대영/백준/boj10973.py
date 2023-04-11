8d2953c003a2b1618645eb99cfabd6f0b24bbaed
Daeyoung Shin <sdy8603@gmail.com>
2023-04-11 21:45:04 +0900

    feature: 10973 문제 풀이 수정

--- 신대영/백준/boj10973.py
+++ 신대영/백준/boj10973.py
@@ -4,11 +4,11 @@ nums = list(map(int, input().split()))
 result = []
 
 for i in range(n - 1, 0, -1):  # 뒤에서부터 비교
-    if nums[i - 1] > nums[i]:  # 앞에 수가 뒤에 수보다 작다면
+    if nums[i - 1] > nums[i]:  # 앞에 수가 뒤에 수보다 크다면
         for j in range(n - 1, 0, -1):  # 다시 그 앞에 값을 맨 뒤값과 비교
-            if nums[i - 1] > nums[j]:
+            if nums[i - 1] > nums[j]:  #
                 nums[i - 1], nums[j] = nums[j], nums[i - 1]
-                result = nums[:i] + sorted(nums[i:])
+                result = nums[:i] + sorted(nums[i:], reverse=True)
                 print(*result)
                 exit()
 
