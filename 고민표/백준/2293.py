de5b388ffd26e1e2b960365329f07d4a4b7bb257
kokoko12334 <gnjgnjgnj1@gmail.com>
2023-04-14 23:25:12 +0900

    20230414코테풀이

--- 고민표/백준/2293.py
+++ 고민표/백준/2293.py
@@ -1,6 +1,4 @@
 
-
-
 n, k = [int(i) for i in input().split()]
 
 coins = [int(input()) for _ in range(n)]
@@ -10,7 +8,6 @@ dp = [0]*(k+1)
 dp[0] = 1
 for i in coins:
     for j in range(i, k+1):
-        if j-i >= 0:
-            dp[j] += dp[j-i]
+        dp[j] += dp[j-i]
 
 print(dp[k])
