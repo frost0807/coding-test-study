10a7b37830af209c30b8d910b89356af20f63219
Jimin <thswlals0219@gmail.com>
2023-04-25 21:27:55 +0900

    feat: 프로그래머스(스킬트리, 점프와 순간 이동)

---
+++ 손지민/Programmers/스킬트리.java
@@ -0,0 +1,19 @@
+package 손지민.Programmers;
+
+public class 스킬트리 {
+    public int learn(String skill, String tree) {
+        for(int i = 0; i < tree.length(); i++) {
+            if(skill.charAt(i) != tree.charAt(i)) return 0;
+        }
+        return 1;
+    }
+
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for(String tree : skill_trees) {
+            answer += learn(skill, tree.replaceAll("[^" + skill + "]", ""));
+        }
+        return answer;
+    }
+
+}
