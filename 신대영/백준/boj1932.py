995ecb4951bf558f75f335a843c83a412b9e180f
Daeyoung Shin <sdy8603@gmail.com>
2023-05-10 22:06:20 +0900

    feature: 백준 1932 문제 풀이 커밋

---
+++ 신대영/백준/boj1932.py
@@ -0,0 +1,13 @@
+N = int(input())
+dp = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(1, N):
+    for j in range(len(dp[i])):
+        if j == 0:
+            dp[i][j] = dp[i][j] + dp[i - 1][j]
+        elif i == j:
+            dp[i][j] = dp[i][j] + dp[i - 1][j - 1]
+        else:
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + dp[i][j]
+
+print(max(dp[N - 1]))
