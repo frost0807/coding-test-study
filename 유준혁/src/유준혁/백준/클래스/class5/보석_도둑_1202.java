898c8e1385b29351c1daca75dce5ededb689d9db
준혁 <junhyeok@event-us.kr>
2023-05-24 21:39:27 +0900

    feat: class 5 2문제

---
+++ 유준혁/src/유준혁/백준/클래스/class5/보석_도둑_1202.java
@@ -0,0 +1,22 @@
+package 유준혁.백준.클래스.class5;
+
+import java.util.*;
+public class 보석_도둑_1202 {
+	public static void main(String[] ar) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), k = sc.nextInt();
+		long sum = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+		List<Integer> bag = new ArrayList<>();
+		List<int[]> jewels = new ArrayList<>();
+		for(int i = 0; i < n; i++) jewels.add(new int[] { sc.nextInt(), sc.nextInt() });
+		for(int i = 0; i < k; i++) bag.add(sc.nextInt());
+		jewels.sort((a, b) -> a[0] - b[0]);
+		bag.sort(null);
+		for(int i = 0, j = 0; i < k; i++) {
+			while(j < n && jewels.get(j)[0] <= bag.get(i)) pq.add(jewels.get(j++)[1]);
+			if (!pq.isEmpty()) sum += pq.poll();
+		}
+		System.out.print(sum);
+	}
+}
